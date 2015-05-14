0336d9f079ccb067fbdb16d14b794bd81ff37c5b
NIX <nix@juanix.net>
2015-05-13 18:29:26 -0700

    branches fix

---
+++ Google Chrome/js/Mi_Youtube_Data.js
@@ -0,0 +1,71 @@
+//save extension data setup
+var ExtensionDataName = 'Mi_Youtube_Data',
+	ExtensionData = {
+	dataVersion: 2.1,
+	accounts: [
+    {
+        //default PMVTutoriales channel
+        id: "ZgwLCu6tSLEUJ30METhJHg", 
+        name: "PMVTutoriales", 
+        thumbnail: "https://i3.ytimg.com/i/ZgwLCu6tSLEUJ30METhJHg/1.jpg?v=516a0349", 
+        videoTitles: [], 
+        newVideos: false,
+        url: "https://www.youtube.com/channel/UCZgwLCu6tSLEUJ30METhJHg"
+    }
+    ],
+    prefs: {
+        'show_popup': true,
+        'play_popup_sound': true,
+        'check_interval': 600000
+    },
+    isNewInstall: true,
+    newVideosCache: []
+};
+
+function DB_setValue(name, value, callback) {
+    var obj = {};
+    obj[name] = value;
+    chrome.storage.local.set(obj, function() {
+        if(callback) callback();
+    });
+}
+
+function DB_load(callback) {
+    chrome.storage.local.get(ExtensionDataName, function(r) {
+        if (isEmpty(r[ExtensionDataName])) {
+            DB_setValue(ExtensionDataName, ExtensionData, callback);
+        } else if (r[ExtensionDataName].dataVersion != ExtensionData.dataVersion) {
+            //update defaults values without losing already existing data to V2.1
+            if (r[ExtensionDataName].newVideosCache === undefined) {
+                r[ExtensionDataName].newVideosCache = [];
+            }
+            r[ExtensionDataName].dataVersion = ExtensionData.dataVersion;
+            ExtensionData = r[ExtensionDataName];
+            DB_setValue(ExtensionDataName, ExtensionData, callback);
+        } else {
+            ExtensionData = r[ExtensionDataName];
+            callback();
+        }
+    });
+}
+
+function DB_save(callback) {
+    DB_setValue(ExtensionDataName, ExtensionData, function() {
+        if(callback) callback();
+    });
+}
+
+function DB_clear(callback) {
+    chrome.storage.local.remove(ExtensionDataName, function() {
+        if(callback) callback();
+    });
+}
+
+function isEmpty(obj) {
+	for (var key in obj) {
+		if (obj.hasOwnProperty(key)) {
+			return false;
+		}
+	}
+	return true;
+}
